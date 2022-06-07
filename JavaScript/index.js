c2064d0da8dfb3e78d436533a5fe870c35385269
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-06-07 13:20:46 +0400

    Feth

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,24 +1,78 @@
-//const nav = document.querySelector("#nav");
-/* console.log(nav); */
-//const navBtn = document.querySelector("#nav-btn");
-/* console.log(navBtn); */
-//const navBtnImg = document.querySelector("#nav-btn-img");
-/* console.log(navBtnImg); */
-
-//navBtn.onclick = () => {
-//if (nav.classList.toggle("open")) {
-//navBtnImg.src = "./img/icons/nav-close.svg";
-/* console.log(navBtnImg); */
-/* console.log(nav); */
-//} else {
-//   navBtnImg.src = "./img/icons/nav-open.svg";
-/* console.log(navBtnImg); */
-/* console.log(nav);*/
-// }
-//};
-const diploms = document.querySelector(".education__diploms");
+/* Для осуществления всплытия дипломов */
+/* const diploms = document.querySelector(".education__diploms");
 const openDiploms = document.querySelector(".education__title");
 
 openDiploms.onclick = () => {
   diploms.classList.toggle("open");
-};
+}; */
+
+/* const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev", 
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+ */
+
+/* new Swiper(".swiper", {
+  //Стрелки
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //Навигация
+  //Буллеты, текущее положение, прогрессбар
+  pagination: {
+    el: ".swiper-pagination",
+    //Буллиты
+    clickable: true,
+  },
+  //Количество отображений слайдев
+  //!!! Так же можно поставить не целой число, а 1.5 например
+  //Данное решение можно будет использовать для реализации на десктопе первого слайдера
+  slidesPerView: 1,
+  //Делаем бесконечную прокрутку
+  loop: true,
+}); */
+
+new Swiper(".swiper", {
+  //Стрелки
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //Навигация
+  //Буллеты, текущее положение, прогрессбар
+  pagination: {
+    el: ".swiper-pagination",
+    //Буллиты
+    clickable: true,
+  },
+  //Количество отображений слайдев
+  //!!! Так же можно поставить не целой число, а 1.5 например
+  //Данное решение можно будет использовать для реализации на десктопе первого слайдера
+  //slidesPerView: 6,
+  //Делаем бесконечную прокрутку
+  /*   : "auto",
+ loopedSlides
+  /* autoHeight: true, */
+  spaceBetween: 30,
+  centeredSliders: true,
+  loop: true,
+  slidesPerView: 3.5,
+});
